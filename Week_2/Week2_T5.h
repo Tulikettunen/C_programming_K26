59ab7cd3b798ac9415183c9bdb5391341a96333c
tuuli <tuuli.mrl@gmail.com>
2026-01-28 18:59:55 +0200

    Created the files for the next weeks tasks.

---
+++ Week_2/Week2_T5.h
@@ -0,0 +1,8 @@
+//
+// Created by tuuli on 28/01/2026.
+//
+
+#ifndef WEEK2_T5_H
+#define WEEK2_T5_H
+
+#endif //WEEK2_T5_H
---
+++ Week_2/Week2_T6.h
@@ -0,0 +1,8 @@
+//
+// Created by tuuli on 28/01/2026.
+//
+
+#ifndef WEEK2_T6_H
+#define WEEK2_T6_H
+
+#endif //WEEK2_T6_H
