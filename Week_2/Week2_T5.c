2d311e8cdbc646033589c20e3673b2bfc0a4fbdc
tuuli <tuuli.mrl@gmail.com>
2026-02-01 23:44:40 +0200

    Copy previous tasks and start fixing earlier errors to make code better. Implement input reading and average calculation functions better way. Tasks Week2_T4 and Week2_T5 fixed. int inpupt funktion still accepts the annoying "4dgdthk" type of input.

--- Week_2/Week2_T5.c
+++ Week_2/Week2_T5.c
@@ -7,19 +7,15 @@
 
 int main(){
     printf("Let's play!\n");
-    int tie = 1;
     int lowest = 1;
     int highest = 6;
     int user_roll;
-    //int count = 0;
 
     for (int i = 0; i < 3; i++) {
-        //count++;
         printf("Enter a number between %d and %d.\n", lowest, highest);
         user_roll = read_range(lowest, highest);
         if (user_roll == highest) {
             printf("I got 6. It is a tie!\n");
-            tie = 0;
         }
         else {
             printf("I got %d. I win!\n", user_roll + 1);
